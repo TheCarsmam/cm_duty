e01fd169afd38ffcd31cb7641ea0e51010472ae4
TheCarsmam <67677131+TheCarsmam@users.noreply.github.com>
2020-07-27 01:06:29 -0300

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,35 @@
+Config                            = {}
+Config.DrawDistance               = 100.0
+--language currently available EN and SV
+Config.Locale                     = 'es' 
+
+Config.Zones = {
+
+  PoliceDuty = {
+    Pos   = { x = 439.825, y = -975.693, z = 29.691 },
+    Size  = { x = 2.5, y = 2.5, z = 1.5 },
+    Color = { r = 0, g = 255, b = 0 },  
+    Type  = 27,
+  },
+
+  AmbulanceDuty = {
+    Pos = { x = 264.45, y = -1356.84, z = 23.56 },
+    Size = { x = 2.5, y = 2.5, z = 1.5 },
+    Color = { r = 0, g = 255, b = 0 },
+    Type = 27,
+  },
+
+  mechanicDuty = {
+    Pos = { x = -321.25, y = -137.99, z = 38.10 },
+    Size = { x = 2.5, y = 2.5, z = 1.5 },
+    Color = { r = 0, g = 255, b = 0 },
+    Type = 27,
+  },
+
+ TaxiDuty = {
+    Pos = { x = 901.2, y = -179.29, z = 73.00 },
+    Size = { x = 2.5, y = 2.5, z = 1.5 },
+    Color = { r = 0, g = 255, b = 0 },
+    Type = 27,
+  },
+}
