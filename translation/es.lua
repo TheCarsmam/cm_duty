5cf10e919cf0730e9e3a1992e28879698497c783
TheCarsmam <67677131+TheCarsmam@users.noreply.github.com>
2020-07-27 01:04:24 -0300

    Delete es.lua

--- translation/es.lua
+++
@@ -1,8 +0,0 @@
-Locales['es'] = {
-  -- regulars
-  	['duty'] = 'Apreta ~INPUT_CONTEXT~ para ~g~entrar~s~/~r~salir~s~ de servicio',
-	['onduty'] = 'Estas en servicio.',
-	['offduty'] = 'Estas fuera de servicio.',
-	['notpol'] = 'No sos policia.',
-	['notamb'] = 'No sos same.',
-}
